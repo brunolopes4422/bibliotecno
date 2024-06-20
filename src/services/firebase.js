9681f7aa66fb764d5b1d16bdf9138369e257e1be
Bruno Lopes Cordeiro <59589058+brunolopes4422@users.noreply.github.com>
2024-06-20 15:18:57 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEGxCKS3_CFVxDZU7Qo2R2fPTjuwSFsCY",
-  authDomain: "projeto1-83f1c.firebaseapp.com",
-  projectId: "projeto1-83f1c",
-  storageBucket: "projeto1-83f1c.appspot.com",
-  messagingSenderId: "806655846722",
-  appId: "1:806655846722:web:45b8525d7ba70339c5"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
